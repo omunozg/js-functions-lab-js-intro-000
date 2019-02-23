59e6f734ac3c91340bad589b641d5068730c8fc3
Oscar Munoz <omunozg@emory.edu>
2019-02-23 21:46:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ return 'Happy holidays, ${name}!'
  }
  
  function happyHolidayTo(holiday, name) {
- console.log ('Happy ${holiday}, ${name}!')
+ return ('Happy ${holiday}, ${name}!')
  }
  
  function holidayCountdown(holiday, days) {
